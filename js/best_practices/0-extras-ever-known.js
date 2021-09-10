a2b830e862b778c5408cbf627bd316c9cef83bbb
tr!nguyen <trinhnguyen959@gmail.com>
2021-09-10 15:58:32 +0700

    commit

--- js/best_practices/0-extras-ever-known.js
+++ js/best_practices/0-extras-ever-known.js
@@ -7,3 +7,20 @@ age > 30
 			: console.log('you getting older')
 		: console.log('you are better than 30 and 59')
 	: console.log('you are bellow 30');
+
+('Get last or n-last element of an array');
+const array = [1, 2, 3, 4, 5, 6, 7];
+console.log(array.slice(-2));
+console.clear();
+
+('Math short hand');
+// floor ~~ lam tron xuong
+let floorNumber = 8.9;
+floorNumber = ~~floorNumber;
+console.log(floorNumber);
+
+// pow ** ham mu
+console.log(2 ** 3);
+
+// a> 21 && doSomething()
+// a = null || ''
