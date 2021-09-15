67597ac4bb59b13eb1b22e502a1e0fbddcbd16eb
tr!nguyen <trinhnguyen959@gmail.com>
2021-09-15 23:57:06 +0700

    a

--- js/form_validation/index.js
+++ js/form_validation/index.js
@@ -106,31 +106,31 @@ function Validator(formSelector) {
 		if (isValid) {
 			if (typeof this.onSubmit.bind(Validator) === 'function') {
 				var enabledInputs = document.querySelectorAll('input[name]');
-					var formValues = Array.from(enabledInputs).reduce((values, input) => {
-						switch (input.type) {
-							case 'radio':
-								values[input.name] = formElement.querySelector(
-									`input[name="${input.name}"]:checked`
-								).value;
-								break;
-							case 'checkbox':
-								if (!input.matches(':checked')) {
-									values[input.name] = [];
-									return values;
-								}
-								if (!Array.isArray(values[input.name])) {
-									values[input.name] = [];
-								}
-								values[input.name].push(input.value);
-								break;
-							case 'file':
-								values[input.name] = input.files;
-								break;
-							default:
-								values[input.name] = input.value;
-						}
-						return values;
-					}, {});
+				var formValues = Array.from(enabledInputs).reduce((values, input) => {
+					switch (input.type) {
+						case 'radio':
+							values[input.name] = formElement.querySelector(
+								`input[name="${input.name}"]:checked`
+							).value;
+							break;
+						case 'checkbox':
+							if (!input.matches(':checked')) {
+								values[input.name] = [];
+								return values;
+							}
+							if (!Array.isArray(values[input.name])) {
+								values[input.name] = [];
+							}
+							values[input.name].push(input.value);
+							break;
+						case 'file':
+							values[input.name] = input.files;
+							break;
+						default:
+							values[input.name] = input.value;
+					}
+					return values;
+				}, {});
 
 				return this.onSubmit.bind(Validator)(formValues);
 			}
