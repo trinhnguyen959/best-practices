12d9288c53556297ff436f61e18516f880febd9f
trinhnv <trinhnguyen959@gmail.com>
2022-04-11 23:46:28 +0700

    finished item one

---
+++ java/effective-java/src/test/java/org/java/ItemOne.java
@@ -0,0 +1,30 @@
+package org.java;
+
+import org.java.classes.Point;
+import org.java.classes.Point.Angle;
+import org.junit.jupiter.api.Test;
+
+// Consider static factory methods instead of constructors
+public class ItemOne {
+	@Test
+	public void staticFactoryMethod() {
+/*		Point pointOne = new Point(3.0, 4.0);
+		System.out.println("pointOne = " + pointOne);
+
+		Point pointTwo = new Point(5.0, 50.0 * Math.PI / 180.0, 8);
+		System.out.println("pointOne = " + pointTwo);
+
+		Point pointThree = new Point(5.0, Math.atan2(4.0, 3.0), Angle.RAD);
+		System.out.println("pointThree = " + pointThree);*/
+
+		Point pointOne = Point.fromCartesian(3.0, 4.0);
+		System.out.println("pointOne = " + pointOne);
+
+		// best practice
+		Point pointThree = Point.fromPolar(5.0, Math.atan2(4.0, 3.0), Angle.RAD);
+		System.out.println("pointThree = " + pointThree);
+
+		Point pointFourth = Point.fromPolar(5.0, 52.13, Angle.DEG);
+		System.out.println("pointFourth = " + pointFourth);
+	}
+}
---
+++ java/effective-java/src/test/java/org/java/classes/Point.java
@@ -0,0 +1,67 @@
+package org.java.classes;
+
+// khong the ke thua
+public class Point {
+	private double x;
+	private double y;
+
+	public enum Angle {
+		RAD,
+		DEG,
+	}
+
+	private Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	// avoid duplicate constructor -> khong nen dung
+	private Point(double rho, double phi, int dummy) {
+		this.x = rho * Math.cos(phi);
+		this.y = rho * Math.sin(phi);
+	}
+
+	// enum
+	private Point(double rho, double phi, Angle angle) {
+		switch (angle) {
+			case RAD -> {
+				this.x = rho * Math.cos(phi);
+				this.y = rho * Math.sin(phi);
+			}
+			case DEG -> {
+				double secondPhi = phi * Math.PI / 180.0;
+				this.x = rho * Math.cos(secondPhi);
+				this.y = rho * Math.sin(secondPhi);
+			}
+		}
+	}
+
+	// static factory method
+	public static Point fromCartesian(double x, double y) {
+		return new Point(x, y);
+	}
+
+	public static Point fromPolar(double rho, double phi, Angle angle) {
+		return new Point(rho, phi, angle);
+	}
+
+	public static Point fromPolarRadian(double rho, double phi) {
+		return new Point(rho, phi, Angle.RAD);
+	}
+
+	public static Point fromPolarDegree(double rho, double phi) {
+		return new Point(rho, phi, Angle.DEG);
+	}
+
+	public static Point newCartesian(double x, double y) {
+		return new Point(x, y);
+	}
+
+	@Override
+	public String toString() {
+		return "Point{" +
+				"x=" + x +
+				", y=" + y +
+				'}';
+	}
+}
