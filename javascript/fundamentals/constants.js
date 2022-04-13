3ec68191d34a40af6cbe28cbfb5471b96b96e542
trinhnv <trinhnguyen959@gmail.com>
2022-04-13 20:07:36 +0700

    remove trash

--- javascript/fundamentals/constants.js
+++
@@ -1,3 +0,0 @@
-export const TYPE_WARNING = 'warning';
-export const TYPE_INFO = 'info';
-export const TYPE_ERROR = 'error';
--- javascript/fundamentals/logger.js
+++
@@ -1,5 +0,0 @@
-import { TYPE_INFO } from './constants.js';
-
-export default function logger(message, type = TYPE_INFO) {
-	console.log(`${message} and type is ${type}`);
-}
