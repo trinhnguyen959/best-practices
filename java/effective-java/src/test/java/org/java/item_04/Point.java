72f343dcbfd133d9422c074d0813ed9ebce0210b
trinhnv <trinhnguyen959@gmail.com>
2022-04-19 22:46:13 +0700

    finished item 04

---
+++ java/effective-java/src/test/java/org/java/item_04/Point.java
@@ -0,0 +1,11 @@
+package org.java.item_04;
+
+public class Point {
+	private int x;
+	private int y;
+
+	protected Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
---
+++ java/effective-java/src/test/java/org/java/item_04/Utils.java
@@ -0,0 +1,10 @@
+package org.java.item_04;
+
+public class Utils {
+	private Utils() {
+	}
+
+	public static Point createCartesianPoint(int x, int y) {
+		return new Point(x, y);
+	}
+}
