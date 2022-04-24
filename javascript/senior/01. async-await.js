fedb5243ba3ed4893336b31f7e336371b1665c99
trinhnv <trinhnguyen959@gmail.com>
2022-04-24 18:04:03 +0700

    refactor

--- javascript/senior/01. async-await.js
+++ javascript/senior/01. async-await.js
@@ -1,9 +1,9 @@
+const handlerRequest = promise =>
+	promise.then(data => [undefined, data]).catch(err => [err, undefined]);
+
 let error, result;
 [error, result] = await handlerRequest('a');
 
 if (error) {
 	console.error('error 1: ', error);
 }
-
-const handlerRequest = promise =>
-	promise.then(data => [undefined, data]).catch(err => [err, undefined]);
