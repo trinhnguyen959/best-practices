7348d91ae617d1cee475d917a2ddaf4966351367
trinhnv <trinhnguyen959@gmail.com>
2022-04-24 19:46:45 +0700

    add lru cache

---
+++ javascript/senior/lru-cache/cache.js
@@ -0,0 +1,33 @@
+class Cache {
+	constructor(size) {
+		this.size = size || 3;
+		this.cache = new Map();
+	}
+
+	put(key, val) {
+		const hasKey = this.cache.has(key);
+		if (hasKey) {
+			this.cache.delete(key);
+		}
+		this.cache.set(key, val);
+		if (this.cache.size > this.size) {
+			this.cache.delete(this.cache.keys().next().value);
+		}
+		return true;
+	}
+
+	get(key) {
+		const hasKey = this.cache.has(key);
+		if (hasKey) {
+			const val = this.cache.get(key);
+			this.cache.delete(key);
+			this.cache.set(key, val);
+			return val;
+		}
+		return -1;
+	}
+
+	item() {
+		return this.cache.entries();
+	}
+}
