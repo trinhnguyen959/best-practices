1bef27405c6d915ab6863361f163bb24a5526b23
trinhnv <trinhnguyen959@gmail.com>
2022-04-24 18:02:45 +0700

    add senior

---
+++ javascript/senior/01. async-await.js
@@ -0,0 +1,9 @@
+let error, result;
+[error, result] = await handlerRequest('a');
+
+if (error) {
+	console.error('error 1: ', error);
+}
+
+const handlerRequest = promise =>
+	promise.then(data => [undefined, data]).catch(err => [err, undefined]);
